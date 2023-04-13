d01bb6ab2f6b269ea7ee38bad971c78248c76d7e
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2023-04-13 15:36:56 +0700

    chore: change segment api host

--- packages/browser/src/core/constants/index.ts
+++ packages/browser/src/core/constants/index.ts
@@ -1,4 +1,4 @@
-export const SEGMENT_API_HOST = 'webapp-demo.primedatacdp.com/api/sdk'
+export const SEGMENT_API_HOST = 'api.segment.io/v1'
 
 // TODO using primedatavn plugin
 export const PRIMEDATA_VN_API_HOST = 'webapp-demo.primedatacdp.com/api/sdk'
